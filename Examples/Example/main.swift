3b7e51ed4778617cae3a259058ab2c5b0a526f62
Elias Haileselassie <eliaszx@gmail.com>
2021-08-18 00:36:14 -0400

    init 2

--- Examples/Example/main.swift
+++ Examples/Example/main.swift
@@ -8,19 +8,66 @@
 import Foundation
 import WeBirr
 
-var api = WeBirr.WeBirrClient(apiKey: "x", isTestEnv: true)
+let apiKey = "YOUR_API_KEY"
+let merchantId = "YOUR_MERCHANT_ID"
 
-/*
+
+createAndUpdateBillAsync()
+
+func createAndUpdateBillAsync() {
+    
+let api = WeBirr.WeBirrClient(apiKey: apiKey, isTestEnv: true)
+
 var bill = Bill(
-customerCode: "cc01",
-customerName: "Elias Haileselassie",
-billReference: "swift/2021/141",
-time: "2021-07-22 22:14",
-description: "hotel booking",
-amount: "270.90",
-merchantID: "x"
+    customerCode: "cc01",
+    customerName: "Elias Haileselassie",
+    billReference: "swift/2021/141",
+    time: "2021-07-22 22:14",
+    description: "hotel booking",
+    amount: "270.90",
+    merchantID: merchantId
 )
- */
 
-print("Hello, World!")
+print("Creating Bill...")
+
+api.createBillAsync(bill: bill) { resp in
+    
+    if  resp.error == nil {
+            // success
+            let paymentCode = resp.res ?? "" // returns paymentcode such as 429 723 975
+            print("Payment Code = \(paymentCode)") // we may want to save payment code in local db.
+
+           } else {
+               // fail
+               print("error: \(resp.error!)")
+               print("errorCode: \(resp.errorCode ?? "")") // can be used to handle specific busines error such as ERROR_INVLAID_INPUT_DUP_REF
+           }
+    
+    //DispatchQueue.main.async {
+    //       updateUIShouldBeHere()!
+    //}
+}
+
+    // the above method call is async!
+Thread.sleep(forTimeInterval: 2)
+    
+    print("Updating Bill...")
+    
+    api.updateBillAsync(bill: bill) { resp in
+
+            if resp.error == nil {
+                // success
+                print("bill is updated successfully")  //it.res will be 'OK'  no need to check here!
+
+            } else {
+                // fail
+                print("error: \(resp.error!)")
+                print("errorCode: \(resp.errorCode ?? "")") // can be used to handle specific busines error such as ERROR_INVLAID_INPUT
+            }
+        }
+    
+    // the above method call is async!
+Thread.sleep(forTimeInterval: 2)
+    
+}
 
--- WeBirr/Sources/WeBirr/Bill.swift
+++ WeBirr/Sources/WeBirr/Bill.swift
@@ -1,11 +1,26 @@
 
-
 public struct Bill : Codable {
-        var customerCode : String
-        var customerName : String
-        var billReference : String
-        var time : String
-        var description : String
-        var amount : String
-        var merchantID : String
+    
+        var customerCode : String = ""
+        var customerName : String = ""
+        var billReference : String = ""
+        var time : String = ""
+        var description : String = ""
+        var amount : String = ""
+        var merchantID : String = ""
+    
+    public init(customerCode : String,customerName : String,billReference : String,
+                time : String, description : String, amount : String, merchantID : String ) {
+        
+        self.customerCode = customerCode
+        self.customerName = customerName
+        self.billReference = billReference
+        self.time = time
+        self.description = description
+        self.amount = amount
+        self.merchantID = merchantID
+        
+        
     }
+    
+}
